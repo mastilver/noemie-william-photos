0b6a7b604e8a26bfd8d72a20da2cd2100165324a
Thomas Sileghem <mastilver@users.noreply.github.com>
2023-05-27 15:00:00 +0100

    tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,49 +14,20 @@ export default function Home() {
 
   return (
     <div className="bg-white">
+      {/* Hero section */}
       <main>
-        {/* Hero section */}
-        <div className="relative isolate overflow-hidden bg-gray-900 pb-16 pt-14 sm:pb-20 brightness-75">
-          <img
-            src="https://picsum.photos/seed/hero/200/300"
-            alt=""
-            className="absolute inset-0 -z-10 h-full w-full object-cover"
-          />
-          <div
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-            />
-          </div>
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-              <div className="text-center">
-                <p className="mt-6 text-lg leading-8 text-gray-300">
-                  Blah blah blah
+        <div className="relative">
+          <img src="/hero.jpg" />
+          
+          <div className="absolute text-2xl text-blue-300 top-5 left-5 px-10 sm:pl-96 sm:pr-40">
+            <div className="text-center">
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                Merci d'avoir partagé cette journee exceptionnelle et merci d'avoir capture les moments inoubliable vous pouvez admirer les oeuvres et mettre les votre.
                 </p>
-                <p className="mt-6 text-lg leading-8 text-gray-400 text-right">
-                  Noemie & William Marechal
+                <p className="mt-6 text-lg leading-8 text-gray-500 text-right">
+                  Noemie & William MARECHAL
                 </p>
-              </div>
             </div>
-          </div>
-          <div
-            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-            />
           </div>
         </div>
 
@@ -75,7 +46,7 @@ export default function Home() {
                   <h3 className="mt-6 text-sm font-semibold leading-3 text-white text-right">
                     <span>
                       <span className="absolute inset-0" />
-                      by {image.author}
+                      par {image.author}
                     </span>
                   </h3>
                 </article>
