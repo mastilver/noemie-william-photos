697ecdfffd3f08ffaed14c5fe220fb37543c9462
Thomas Sileghem <mastilver@users.noreply.github.com>
2023-05-27 15:06:59 +0100

    add button to upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,27 +53,30 @@ export default function Home() {
               ))}
             </div>
             <nav className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
-      <div className="-mt-px flex w-0 flex-1 py-3">
-        <a
-          href="#"
-          className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-        >
-          <ArrowLongLeftIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-          Précédent
-        </a>
-      </div>
-      <div className="-mt-px flex w-0 flex-1 justify-end">
-        <a
-          href="#"
-          className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-        >
-          Suivant
-          <ArrowLongRightIcon className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-        </a>
-      </div>
-    </nav>
+              <div className="-mt-px flex w-0 flex-1 py-3">
+                <a
+                  href="#"
+                  className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                >
+                  <ArrowLongLeftIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                  Précédent
+                </a>
+              </div>
+              <div className="-mt-px flex w-0 flex-1 justify-end">
+                <a
+                  href="#"
+                  className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                >
+                  Suivant
+                  <ArrowLongRightIcon className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                </a>
+              </div>
+            </nav>
           </div>
         </div>
+
+        <button title="Ajouter vos photos"
+        className="fixed z-90 bottom-14 right-2 bg-blue-600 py-2 px-1 text-lg rounded-full drop-shadow-lg justify-center items-center text-white hover:bg-blue-700 hover:drop-shadow-2xl">Ajouter vos photos</button>
       </main>
     </div>
   )
