506845a60efa4cc0d701eb1bec51d03d55fcb9c6
Thomas Sileghem <mastilver@users.noreply.github.com>
2023-05-31 18:02:43 +0100

    improve images display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 "use client"
 
-import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/react/20/solid'
+import { ArrowLongLeftIcon, ArrowLongRightIcon, ArrowDownIcon } from '@heroicons/react/20/solid'
 import { useS3Upload } from "next-s3-upload";
 
 export default function Home() {
   const {
-    files,
+    files: uploadingFiles,
     openFileDialog,
     uploadToS3,
   } = useS3Upload()
-
-  console.log(files)
 
   async function handleFileChange(files: File[]) {
     for(const file of files) {
@@ -18,14 +16,46 @@ export default function Home() {
     }
   }
 
+  function downloadFile(url: string) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = '';
+  
+    link.click();
+  }
+
 
   const images = [
+    {
+      url: 'https://telemediabroadcasting.com/wp-content/uploads/2016/03/WhiteBackground.jpg',
+      author: 'Thomas',
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Black_colour.jpg',
+      author: 'Thomas',
+    },
+    {
+      url: 'https://picsum.photos/seed/1/300/200',
+      author: 'Thomas',
+    },
+    {
+      url: 'https://picsum.photos/seed/2/300/200',
+      author: 'Thomas',
+    },
+    {
+      url: 'https://picsum.photos/seed/1/300/200',
+      author: 'Thomas',
+    },
+    {
+      url: 'https://picsum.photos/seed/2/200/300',
+      author: 'Thomas',
+    },
     {
       url: 'https://picsum.photos/seed/1/200/300',
       author: 'Thomas',
     },
     {
-      url: 'https://picsum.photos/seed/2/200/300',
+      url: 'https://picsum.photos/seed/2/300/200',
       author: 'Thomas',
     }
   ]
@@ -49,49 +79,46 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="bg-white py-0 sm:py-1">
-          <div className="mx-auto px-1 lg:px-1">
-            <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-              {images.map((image) => (
-                <article
-                  key={image.url}
-                  className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-4 pb-4 pt-80 sm:pt-48 lg:pt-80"
-                >
-                  <img src={image.url} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
-                  <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-                  <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-
-                  <h3 className="mt-6 text-sm font-semibold leading-3 text-white text-right">
-                    <span>
-                      <span className="absolute inset-0" />
-                      par {image.author}
-                    </span>
-                  </h3>
-                </article>
-              ))}
-            </div>
-            <nav className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
-              <div className="-mt-px flex w-0 flex-1 py-3">
-                <a
-                  href="#"
-                  className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                >
-                  <ArrowLongLeftIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  Précédent
-                </a>
+        <div className="container mx-auto py-0 sm:py-1">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-1 lg:px-8">
+            {images.map((image) => (
+              <div className="md:col-span-1">
+                <div className="relative">
+                  <img src={image.url} className="w-full h-auto"/>
+                  <span className="absolute bottom-0 right-0 bg-white text-black p-2 text-sm font-semibold rounded-tl-md">
+                    <p className='inline-block'>par Thomas</p>
+                    <ArrowDownIcon
+                      className="inline-block h-5 w-5 text-black cursor-pointer"
+                      title='Télécharger'
+                      onClick={() => downloadFile(image.url)}
+                    />
+                  </span>
+                </div>
               </div>
-              <div className="-mt-px flex w-0 flex-1 justify-end">
-                <a
-                  href="#"
-                  className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                >
-                  Suivant
-                  <ArrowLongRightIcon className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                </a>
-              </div>
-            </nav>
+            ))}         
           </div>
         </div>
+
+        <nav className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
+          <div className="-mt-px flex w-0 flex-1 py-3">
+            <a
+              href="#"
+              className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            >
+              <ArrowLongLeftIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+              Précédent
+            </a>
+          </div>
+          <div className="-mt-px flex w-0 flex-1 justify-end">
+            <a
+              href="#"
+              className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            >
+              Suivant
+              <ArrowLongRightIcon className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+            </a>
+          </div>
+        </nav>
 
         <label
           title="Ajouter vos photos"
