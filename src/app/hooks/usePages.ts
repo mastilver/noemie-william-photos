161fba1b1474ccb242e64f28e67e925f58999aa1
Thomas Sileghem <mastilver@users.noreply.github.com>
2023-06-01 21:52:02 +0100

    refactor

---
+++ src/app/hooks/usePages.ts
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react'
+import ky from 'ky'
+
+type Content = {
+    url: string,
+    author: string,
+    type: 'image' | 'video',
+}
+
+type Page = {
+    nextContinuationToken: string | null,
+    contents: Content[]
+}
+
+export default function usePages(): {
+    contents: Content[],
+    currentPage: number,
+    reset(): Promise<void>,
+    previousPageDisabled: boolean,
+    goToPreviousPage(): Promise<void>,
+    nextPageDisabled: boolean,
+    goToNextPage(): Promise<void>,
+} {
+    const [currentPage, setCurrentPage] = useState(0)
+
+    const [pages, setPages] = useState<Page[]>([])
+
+    useEffect(() => {
+        queryMoreFiles()
+    }, [])
+
+    async function queryMoreFiles() {
+        const currentArrayLength = pages.length
+        const nextContinuationToken = pages[currentArrayLength - 1]?.nextContinuationToken ?? '';
+
+        // If end of data or data already there
+        if (currentArrayLength !== 0 && (pages[currentArrayLength] || !nextContinuationToken)) {
+        return
+        }
+
+        const data = await ky.get(`/api/contents?nextContinuationToken=${nextContinuationToken}`).json<Page>()
+
+        setPages(arr => {
+        // NOTE: use previously queried length to prevent race conditions
+        arr[currentArrayLength] = data
+        return Array.from(arr)
+        })
+    }
+
+    async function goToPreviousPage() {
+        if (currentPage !== 0) {
+        setCurrentPage(currentPage - 1)
+        }
+    }
+
+    async function goToNextPage() {
+        const page = pages[currentPage]
+
+        if (page?.nextContinuationToken) {
+        setCurrentPage(currentPage + 1)
+        }
+
+        queryMoreFiles()
+    }
+
+    async function reset() {
+        setCurrentPage(0)
+        setPages([])
+        await queryMoreFiles()
+    }
+
+    return {
+        contents: pages[currentPage]?.contents ?? [],
+        reset,
+        currentPage,
+        previousPageDisabled: currentPage === 0,
+        goToPreviousPage,
+        nextPageDisabled: pages[currentPage]?.nextContinuationToken != null,
+        goToNextPage,
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,8 @@
 
 import { ArrowLongLeftIcon, ArrowLongRightIcon, ArrowDownIcon } from '@heroicons/react/20/solid'
 import { useS3Upload } from "next-s3-upload";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
-import ky from 'ky'
+
+import usePages from './hooks/usePages';
 
 export default function Home() {
   const {
@@ -116,83 +109,4 @@ export default function Home() {
       </main>
     </div>
   )
-}
-
-type Content = {
-  url: string,
-  author: string,
-  type: 'image' | 'video',
-}
-
-type Page = {
-  nextContinuationToken: string | null,
-  contents: Content[]
-}
-
-function usePages(): {
-  contents: Content[],
-  currentPage: number,
-  reset(): Promise<void>,
-  previousPageDisabled: boolean,
-  goToPreviousPage(): Promise<void>,
-  nextPageDisabled: boolean,
-  goToNextPage(): Promise<void>,
-} {
-  const [currentPage, setCurrentPage] = useState(0)
-
-  const [pages, setPages] = useState<Page[]>([])
-
-  useEffect(() => {
-    queryMoreFiles()
-  }, [])
-
-  async function queryMoreFiles() {
-    const currentArrayLength = pages.length
-    const nextContinuationToken = pages[currentArrayLength - 1]?.nextContinuationToken ?? '';
-
-    // If end of data or data already there
-    if (currentArrayLength !== 0 && (pages[currentArrayLength] || !nextContinuationToken)) {
-      return
-    }
-
-    const data = await ky.get(`/api/contents?nextContinuationToken=${nextContinuationToken}`).json<Page>()
-
-    setPages(arr => {
-      // NOTE: use previously queried length to prevent race conditions
-      arr[currentArrayLength] = data
-      return Array.from(arr)
-    })
-  }
-
-  async function goToPreviousPage() {
-    if (currentPage !== 0) {
-      setCurrentPage(currentPage - 1)
-    }
-  }
-
-  async function goToNextPage() {
-    const page = pages[currentPage]
-
-    if (page?.nextContinuationToken) {
-      setCurrentPage(currentPage + 1)
-    }
-
-    queryMoreFiles()
-  }
-
-  async function reset() {
-    setCurrentPage(0)
-    setPages([])
-    await queryMoreFiles()
-  }
-
-  return {
-    contents: pages[currentPage]?.contents ?? [],
-    reset,
-    currentPage,
-    previousPageDisabled: currentPage === 0,
-    goToPreviousPage,
-    nextPageDisabled: pages[currentPage]?.nextContinuationToken != null,
-    goToNextPage,
-  }
 }
