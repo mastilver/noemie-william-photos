750590bf854d802f4c616a236a4a6b20c87f939f
Thomas Sileghem <mastilver@users.noreply.github.com>
2023-06-02 11:11:54 +0100

    add login screen layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,82 @@ import useAuth from './hooks/useAuth';
 
 export default function Home() {
   const {
+    isLoggedIn,
+    login,
+  } = useAuth()
+
+  if (!isLoggedIn) {
+    // TODO: login screen
+    return (
+      <LogInPage login={login}/>
+    )
+  }
+
+  return (
+    <MainPage />
+  )
+}
+
+
+function LogInPage({
+  login,
+}: {
+  login(username: string, password: string): any,
+}) {
+  return (
+    <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label htmlFor="email" className="block text-base font-medium leading-3 text-gray-900">
+              Nom <span className="text-sm">(utilisé seulement pour poster une photo)</span>
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Mot de passe partagé
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Se connecter
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+function MainPage() {
+  const {
     files: uploadingFiles,
     openFileDialog,
     uploadToS3,
@@ -21,10 +97,6 @@ export default function Home() {
     goToNextPage,
   } = usePages()
 
-  const {
-    isLoggedIn,
-  } = useAuth()
-
   async function handleFileChange(files: File[]) {
     for(const file of files) {
       await uploadToS3(file);
@@ -37,11 +109,6 @@ export default function Home() {
     link.download = '';
   
     link.click();
-  }
-
-  if (!isLoggedIn) {
-    // TODO: login screen
-    return null
   }
 
   return (
